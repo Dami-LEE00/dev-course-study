7c74360a77412fd95276e358c314ee03ea7f0a62
“Dami-LEE00” <dxmxxii@gmail.com>
2024-03-27 00:03:20 +0900

    feat: router 처리 및 req.body에서 userId 추출 기능 구현

---
+++ youtube-project/routes/channel.js
@@ -0,0 +1,115 @@
+// express 모듈 셋팅
+const express = require('express')
+const router = express.Router();
+
+router.use(express.json())  // http 외 모듈 'json'
+
+const channelDB = new Map()
+let id = 1; // 하나의 객체를 유니크하게 구별하기 위함
+
+router
+  .route('/')
+  // 채널 전체 조회
+  .get((req, res) => {
+    const { userId } = req.body
+    let channels = []
+    let channels_id = []
+
+    if(channelDB.size) {
+      channelDB.forEach((value) => {
+        channels.push(value)
+      })
+      // channels 배열에 값이 들어있으면 정상 반환 / 없으면 오류 반환
+      if(channels.length) {
+        // req.body에 userId가 따로 지정되어있는 경우, 해당 userId가 생성한 채널만 반환
+        if(userId) {
+          channelDB.forEach((value) => {
+            if(value.userId === userId) {
+              channels_id.push(value)
+            }
+          })
+          res.status(200).json(channels_id)
+        // userId를 따로 요청하지 않은 경우, 모든 채널 반환
+        } else {
+          res.status(200).json(channels)
+        }
+      } else {
+        notFoundChannel(res)
+      }
+    } else {
+      notFoundChannel(res)
+    }
+  })
+  // 채널 개별 생성
+  .post((req, res) => {
+    let { channelTitle, userId } = req.body
+
+    if(channelTitle && userId) {
+      channelDB.set(id++, req.body)
+      res.status(201).json({
+        message: `${userId}님, ${channelTitle} 채널 개설을 환영합니다!`
+      })
+    } else {
+      res.status(400).json({
+        error: '요청 값을 제대로 보내주세요.'
+      })
+    }
+  })
+
+router
+  .route('/:id')
+  // 채널 개별 조회
+  .get((req, res) => {
+    let { id } = req.params
+    id = parseInt(id)
+
+    let channel = channelDB.get(id)
+    if(channel) {
+      res.status(200).json(channel)
+    } else {
+      notFoundChannel(res)
+    }
+  })
+  // 채널 개별 수정
+  .put((req, res) => {
+    let { id } = req.params
+    id = parseInt(id)
+
+    let channel = channelDB.get(id)
+    let oldTitle = channel.channelTitle
+
+    if(channel) {
+      let newTitle = req.body.channelTitle
+      channel.channelTitle = newTitle
+      
+      channelDB.set(id, channel)
+      res.json({
+        message: `채널명이 ${oldTitle}에서 ${newTitle}로 변경되었습니다.`
+      })
+    } else {
+      notFoundChannel(res)
+    }
+  })
+  // 채널 개별 삭제
+  .delete((req, res) => {
+    let { id } = req.params
+    id = parseInt(id)
+
+    let channel = channelDB.get(id)
+    if(channel) {
+      channelDB.delete(id)
+      res.status(200).json({
+        message: `${channel.channelTitle} 채널이 정상적으로 삭제되었습니다.`
+      })
+    } else {
+      notFoundChannel(res)
+    }
+  })
+
+const notFoundChannel = (res) => {
+  res.status(404).json({
+    error: '채널 정보를 찾을 수 없습니다.'
+  })
+}
+
+module.exports = router
---
+++ youtube-project/routes/user.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const router = express.Router()
+
+router.use(express.json())
+
+const userDB = new Map()
+
+// 로그인
+router.post('/login', (req, res) => {
+  // userId가 DB에 저장된 회원인지 확인
+  const { userId, password } = req.body
+  userDB.forEach((user, id) => {
+    if(user.userId === userId) {
+      if(user.password === password) {
+        res.json({
+          message: `${userId}님, 로그인에 성공하셨습니다.`
+        })
+      } else {
+        res.status(401).json({
+          error: '비밀번호가 틀렸습니다. 다시 시도해주세요.'
+        })
+      }
+    } else {
+      res.status(401).json({
+        error: '존재하지 않는 id입니다. 회원가입 먼저 진행해주세요.'
+      })
+    }
+  })
+});
+
+// 회원가입
+router.post('/join', (req, res) => {
+  const { userId, password, name } = req.body
+  if(userId && password && name) {
+    userDB.set(userId, req.body)
+    res.status(201).json({
+      message: `${name}님 환영합니다.`
+    });
+  } else {
+    res.status(400).json({
+      error: 'id, password, name을 모두 입력해주세요.'
+    });
+  }
+});
+
+router
+  .route('/users')
+  // 회원 정보 전체 & 개별 조회
+  .get((req, res) => {
+    let { userId } = req.body
+    // req.body가 비어있을 경우 전체 조회
+    if(!userId) {
+      let users = []
+      userDB.forEach((value) => {
+        users.push({
+          userId: value.userId,
+          name: value.name
+        })
+      })
+      res.status(200).json(users)
+      // req.body에 userId가 있는 경우 해당 회원 개별 조회
+    } else {
+      const user = userDB.get(userId)
+      if(user) {
+        res.status(200).json({
+          userId: user.userId,
+          name: user.name
+        })
+      } else {
+        notFoundUser(res)
+      }
+    }
+  
+  })
+  // 회워 정보 개별 삭제
+  .delete((req, res) => {
+    let { userId } = req.body
+  
+    const user = userDB.get(userId)
+    if(user) {
+      userDB.delete(userId)
+      res.status(200).json({
+        message: `${user.name}님, 다음에 또 뵙겠습니다.`
+      })
+    } else {
+      notFoundUser(res)
+    }
+  })
+
+const notFoundUser = (res) => {
+  res.status(404).json({
+    error: '회원 정보를 찾을 수 없습니다.'
+  })
+}
+
+module.exports = router
