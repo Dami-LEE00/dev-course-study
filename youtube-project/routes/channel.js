7ed8e9e26ff8b199524a8dfaec81db68d61919a3
“Dami-LEE00” <dxmxxii@gmail.com>
2024-04-02 01:16:24 +0900

    feat : 채널 전체 조회 및 개별 조회,생성,삭제 기능 db 연동하여 구현

--- youtube-project/routes/channel.js
+++ youtube-project/routes/channel.js
@@ -1,72 +1,47 @@
 // express 모듈 셋팅
 const express = require('express')
 const router = express.Router();
+const conn = require('../mariadb')
 
-router.use(express.json())  // http 외 모듈 'json'
-
-const channelDB = new Map()
-let id = 1; // 하나의 객체를 유니크하게 구별하기 위함
+router.use(express.json()) 
 
 router
   .route('/')
-  // 채널 전체 조회
+  // 채널 전체 & 각 사용자마다 조회
   .get((req, res) => {
     const { userId } = req.body
-    let channels = []
-    let channels_id = []
 
-    if (!channelDB.size) {
-      return notFoundChannel(res);
+    if(!userId) {
+      let sql = `SELECT * FROM channels`
+      conn.query(sql,
+        function (err, results) {
+          results.length ? res.status(200).json(results) : notFoundChannel(res)
+        }
+      )
+    } else {
+      let sql = `SELECT * FROM channels WHERE user_id = ?`
+      conn.query(sql, userId,
+        function (err, results) {
+          results.length ? res.status(200).json(results) : notFoundChannel(res)
+        }
+      )
     }
-    
-    channelDB.forEach((value) => {
-      // userId가 요청되었을 때 해당 userId의 채널만 반환
-      if (userId && value.userId === userId) {
-        channels_id.push(value);
-      } else {
-        channels.push(value);
-      }
-    });
-    
-    // channels_id가 존재하면 해당 채널 반환, 없으면 모든 채널 반환
-    const resultChannels = channels_id.length ? channels_id : channels;
-    
-    res.status(200).json(resultChannels);
-
-    // if(channelDB.size) {
-    //   channelDB.forEach((value) => {
-    //     channels.push(value)
-    //   })
-    //   // channels 배열에 값이 들어있으면 정상 반환 / 없으면 오류 반환
-    //   if(channels.length) {
-    //     // req.body에 userId가 따로 지정되어있는 경우, 해당 userId가 생성한 채널만 반환
-    //     if(userId) {
-    //       channelDB.forEach((value) => {
-    //         if(value.userId === userId) {
-    //           channels_id.push(value)
-    //         }
-    //       })
-    //       res.status(200).json(channels_id)
-    //     // userId를 따로 요청하지 않은 경우, 모든 채널 반환
-    //     } else {
-    //       res.status(200).json(channels)
-    //     }
-    //   } else {
-    //     notFoundChannel(res)
-    //   }
-    // } else {
-    //   notFoundChannel(res)
-    // }
   })
   // 채널 개별 생성
   .post((req, res) => {
-    let { channelTitle, userId } = req.body
+    const { name, userId } = req.body
 
-    if(channelTitle && userId) {
-      channelDB.set(id++, req.body)
-      res.status(201).json({
-        message: `${userId}님, ${channelTitle} 채널 개설을 환영합니다!`
-      })
+    if(name && userId) {
+      let sql = `INSERT INTO channels (name, user_id) VALUES (?, ?)`
+      let values = [name, userId]
+      conn.query(sql, values,
+        function (err, results) {
+          res.status(201).json(results)
+          res.status(201).json({
+            message: `${userId}님, ${name} 채널 개설을 환영합니다!`
+          })
+        }
+      )
     } else {
       res.status(400).json({
         error: '요청 값을 제대로 보내주세요.'
@@ -81,12 +56,12 @@ router
     let { id } = req.params
     id = parseInt(id)
 
-    let channel = channelDB.get(id)
-    if(channel) {
-      res.status(200).json(channel)
-    } else {
-      notFoundChannel(res)
-    }
+    let sql = `SELECT * FROM channels WHERE id = ?`
+    conn.query(sql, id,
+      function (err, results) {
+        results.length ? res.status(200).json(results) : notFoundChannel(res)
+      }
+    )
   })
   // 채널 개별 수정
   .put((req, res) => {
@@ -113,15 +88,12 @@ router
     let { id } = req.params
     id = parseInt(id)
 
-    let channel = channelDB.get(id)
-    if(channel) {
-      channelDB.delete(id)
-      res.status(200).json({
-        message: `${channel.channelTitle} 채널이 정상적으로 삭제되었습니다.`
-      })
-    } else {
-      notFoundChannel(res)
-    }
+    let sql = `DELETE FROM channels WHERE id = ?`
+    conn.query(sql, id,
+      function (err, results) {
+        results.affectedRows ? res.status(200).json(results) : notFoundChannel(res)
+      }
+    )
   })
 
 const notFoundChannel = (res) => {
