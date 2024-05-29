35cec6bf88bf16e9390ea888d4dabbb14eb620ce
“Dami-LEE00” <dxmxxii@gmail.com>
2024-05-30 00:03:20 +0900

    design: GlobalStyle 및 테마 구현

---
+++ book-store-project/src/style/global.ts
@@ -0,0 +1,34 @@
+import "sanitize.css";
+import { createGlobalStyle } from "styled-components";
+import { ThemeName } from "./theme";
+
+interface Props {
+  themeName: ThemeName
+}
+
+export const GlobalStyle = createGlobalStyle<Props>`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: ${(props) => props.themeName === 'light' ? 'black' : 'white'};
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: ${(props) => props.themeName === 'light' ? 'white' : 'black'};
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  h1 {
+    margin: 0;
+  }
+`;
