7468483375a5a31d77224dafb2190fef7658c8d9
“Dami-LEE00” <dxmxxii@gmail.com>
2024-05-31 04:06:21 +0900

    feat: Home 컴포넌트에 Title, Button, InputText 컴포넌트 추가

--- book-store-project/src/pages/Home.tsx
+++ book-store-project/src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Title from '../components/common/Title';
+import Button from '../components/common/Button';
+import InputText from '../components/common/InputText';
 
 const Home = () => {
   return (
     <div>
-      <Title size='medium' color='third'>제목 테스트</Title>
+      <Title size='medium' color='third'>타이틀 테스트</Title>
+      <Button
+        size='large'
+        schema='normal'
+        disabled={false}
+        isLoading={false}
+      >버튼 테스트</Button>
+      <InputText placeholder='여기에 입력하세요' />
       <div>Home Compo</div>
     </div>
   )
